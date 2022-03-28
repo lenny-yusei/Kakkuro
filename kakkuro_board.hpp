04f84dd79a930d5d8ffc41bfe0e8e1712de3ae60
lenny-yusei <lennard_danny@icloud.com>
2022-03-28 16:58:41 +0900

    add Point class

--- kakkuro_board.hpp
+++ kakkuro_board.hpp
@@ -1,10 +1,25 @@
 #ifndef _KAKKURO_BOARD_HPP_
 #define _KAKKURO_BOARD_HPP_
 
+#include <vector>
+
 /*
 ボードサイズは可変
-上辺、左辺に数字用の黒マスがあるので 0 ~ h の h+1 サイズとする
-表示をどうするか　かなり横に長くなりそう  |rr/dd|  n  |* / *| みたいな    
+上辺、左辺に数字用の黒マスがあるので 0 ~ h の h+1 サイズにする
+表示をどうするか　かなり横に長くなりそう  |rr/dd|  n  |* / *| みたいな
+各空きマスは縦横それぞれどこかのマス群に属してる　それぞれの属してるグループへの参照が必要
+コピーも考えるとポインタではなく各行列の中の群ベクトルのインデックス
 */
+
+class Point {
+public:
+    int x;
+    int y;
+    Point() : x(0), y(0) {}
+    Point(const int _x, const int _y) : x(_x), y(_y) {}
+    Point(const Point& p) : x(p.x), y(p.y) {}
+};
+
+
 
 #endif
